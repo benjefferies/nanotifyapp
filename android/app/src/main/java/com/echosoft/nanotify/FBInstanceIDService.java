7c059ee2e8f593fc05452cad45deec51b2721c0f
Jack Jefferies <jack.h.jefferies@gmail.com>
2018-02-26 23:21:39 +0000

    initial commit without google service json, can now receive notifications when backgrounded from FCM

---
+++ android/app/src/main/java/com/echosoft/nanotify/FBInstanceIDService.java
@@ -0,0 +1,19 @@
+package com.echosoft.nanotify;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class FBInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "FBInstanceIDService";
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.e(TAG, "Refreshed token: " + refreshedToken);
+
+    }
+}
---
+++ android/app/src/main/java/com/echosoft/nanotify/FBMessagingService.java
@@ -0,0 +1,8 @@
+package com.echosoft.nanotify;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class FBMessagingService extends FirebaseMessagingService {
+
+
+}
