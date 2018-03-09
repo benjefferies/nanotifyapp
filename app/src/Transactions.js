51e9110549cb1b74b06a839a275145adce8c1410
Ben Jefferies <benjefferies@echosoft.uk>
2018-03-09 16:08:31 +0000

    Tidy up string and card

--- app/src/Transactions.js
+++ app/src/Transactions.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Image, View, Button, TextInput, AsyncStorage, Text} from 'react-native';
+import {StyleSheet, Image, ScrollView, Button, TextInput, AsyncStorage, Text} from 'react-native';
 import QRCode from 'react-native-qrcode';
 import axios from 'axios';
 import CardView from 'react-native-cardview'
@@ -32,65 +32,25 @@ export default class Transactions extends React.Component {
 
     render() {
         return (
-            <View style={styles.containerFullWith}>
+            <ScrollView>
                 {this.state.transactions.map((prop, i) => {
                     return (
-                        <CardView
+                        <CardView key={'card-' + i} style={styles.itemContainer}
                                 cardElevation={2}
                                 cardMaxElevation={2}
                                 cornerRadius={5}>
-                                <Text key={'amount-' + i}>Received {prop.amount}</Text>
-                                <Text key={'account-' + i}>{prop.account}</Text>
+                                <Text key={'account-' +i}>{prop.account}</Text>
+                                <Text key={'amount-' + i}>{parseFloat(prop.amount /= 1.0e+30).toFixed(5)} NANO</Text>
                         </CardView>
                     );
                 })}
-            </View>
+            </ScrollView>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    containerCentered: {
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    containerFullWith: {
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
-    },
-    logo: {
-        height: 80,
-        marginBottom: 16,
-        width: 80,
-    },
-    button: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
-    modules: {
-        margin: 20,
-    },
-    modulesHeader: {
-        fontSize: 16,
-        marginBottom: 8,
-    },
-    module: {
-        fontSize: 14,
-        marginTop: 4,
-        textAlign: 'center',
+    itemContainer:{
+        padding: 20
     }
 });
